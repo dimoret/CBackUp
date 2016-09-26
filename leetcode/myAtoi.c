a3117677b24df7bab452e64e8cbdeab95503429c
dimoret <ppripples@icloud.com>
2016-09-26 22:20:46 +0800

    Update myAtoi.c

--- leetcode/myAtoi.c
+++ leetcode/myAtoi.c
@@ -1,4 +1,10 @@
+/*
+Implement atoi to convert a string to an integer.
 
+Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+
+Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
+*/
 int myAtoi(char* str) {
 	long long int symbol = 1;
 	long long int result = 0;
