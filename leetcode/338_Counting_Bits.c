51ab01b9780eca4ba48141ca2a371b27e8588dd0
dimoret <ppripples@icloud.com>
2016-10-01 15:34:15 +0800

    Update 338_Counting_Bits.c

--- leetcode/338_Counting_Bits.c
+++ leetcode/338_Counting_Bits.c
@@ -1,3 +1,15 @@
+/*
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+*/
 int* countBits(int num, int* returnSize) {
 	int i = 0;
 	int *BitArray = (int*)malloc((num+1)*sizeof(int));//BitArray[i] store the numbers of the 1 in i
@@ -9,3 +21,6 @@ int* countBits(int num, int* returnSize) {
 	*returnSize = num + 1;
 	return BitArray;
 }	
+/*
+AC:23ms
+*/
