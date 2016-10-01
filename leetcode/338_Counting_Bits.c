54b32981cf67f1535b6dc03209cc1998b7a0ad54
dimoret <ppripples@icloud.com>
2016-10-01 15:32:02 +0800

    Update 338_Counting_Bits.c

--- leetcode/338_Counting_Bits.c
+++ leetcode/338_Counting_Bits.c
@@ -1,34 +1,11 @@
-
 int* countBits(int num, int* returnSize) {
-	int i = 0;//all numbers that <= num
-	int *BitArray = (int*)malloc((num-1)*sizeof(int));//BitArray[i] store the numbers of the 1 in i
-	
-	for(i;i<=num;i++)
-	{
-		if(i == 0)
-		{
-			BitArray[0] = 0;
-			return BitArray;
-		}
-		if(i == 1)
-		{
-			
-		}
-		int j = 0;
-		int k = 0;
-		j = i;
-		k = i;
-		int BitLength = 0;//the length of all the bit string(include 0 and 1)
-		while(j)
-		{
-			j = j>>1;
-			BitLength++;
-		}
-		while(k)
-		{
-			k = k - 2<<(BitLength - 2);
-			BitArray[i]++;
-		}
+	int i = 0;
+	int *BitArray = (int*)malloc((num+1)*sizeof(int));//BitArray[i] store the numbers of the 1 in i
+	BitArray[0] = 0;
+	for(i;i <= num;i++)
+	{	
+		BitArray[i] = BitArray[i>>1] + (i&1);
 	}
+	*returnSize = num + 1;
 	return BitArray;
 }	
